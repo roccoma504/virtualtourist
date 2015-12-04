aa6b87d8e059f13935ff74d7ec768ab9ed762e86
Matt Rocco <roccoma504@gmail.com>
2015-12-03 19:18:46 -0500

    added map view controller

--- MapViewController.swift
+++ MapViewController.swift
@@ -1,25 +1,26 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  virtualtourist
 //
 //  Created by Matthew Rocco on 12/3/15.
 //  Copyright © 2015 Matthew Rocco. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
 
+class MapViewController : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
