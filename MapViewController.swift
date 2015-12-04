445f69395b67b0f241d16fe9edac8b3eced97edf
Matt Rocco <roccoma504@gmail.com>
2015-12-03 19:29:38 -0500

    updating for testing

--- MapViewController.swift
+++ MapViewController.swift
@@ -7,10 +7,14 @@
 //
 
 import Foundation
+import MapKit
 import UIKit
 
 
 class MapViewController : UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
