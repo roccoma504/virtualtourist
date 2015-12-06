776c5183f186d651be16b5280e92797da03b6a55
Matt Rocco <roccoma504@gmail.com>
2015-12-06 12:18:08 -0500

    pin drops

--- MapViewController.swift
+++ MapViewController.swift
@@ -22,11 +22,50 @@ class MapViewController : UIViewController, MKMapViewDelegate {
         mapView.delegate = self
         // Load and configure the mapview.
         loadMapConfig()
+        
+        addOnUserTapAction(mapView: mapView, target:self, action: "addAnnotation:")
+
     }
     
     override func viewDidDisappear(animated: Bool) {
         setMapConfig()
     }
+    
+    func addOnUserTapAction(mapView mapView: MKMapView, target: AnyObject, action: Selector, tapDuration: Double = 1){
+        
+        // Adding a colon at the end of the selector we pass the recognizer that triggered the action to the action
+        let longPressRecognizer = UILongPressGestureRecognizer(target: target, action: action)
+        longPressRecognizer.minimumPressDuration = tapDuration
+        
+        // Add gesture recognizer to the map
+        mapView.addGestureRecognizer(longPressRecognizer)
+    }
+    
+    func getTappedLocation(mapView mapView: MKMapView, gestureRecognizer: UIGestureRecognizer) -> CLLocationCoordinate2D{
+        
+        // Get the position on the screen where the user pressed, relative to the mapView
+        let touchPoint = gestureRecognizer.locationInView(mapView)
+        
+        // Get location coordinate in map
+        return mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+    }
+    
+    func addAnnotation(gestureRecognizer:UIGestureRecognizer){
+        print("add annotation")
+        
+        let location = getTappedLocation(mapView: mapView, gestureRecognizer: gestureRecognizer)
+        
+        // Add an annotation
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location;
+        annotation.title = "You created this annotation!"
+        mapView.addAnnotation(annotation)
+        mapView.removeGestureRecognizer(mapView.gestureRecognizers![0])
+        addOnUserTapAction(mapView: mapView, target:self, action: "addAnnotation:")
+
+    }
+    
+
     
     /**
      Configure the mapview based on what is in defaults.
